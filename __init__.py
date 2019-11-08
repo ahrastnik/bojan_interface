55bb842dd60a02db61391881406fbc3169c406c7
Jaka Kovše <jk4684@student-uni-lj.si>
2019-11-08 14:31:01 +0100

    basic bojan ju aj

--- __init__.py
+++ __init__.py
@@ -3,12 +3,30 @@ Bojan interface
 
 NEMI - 2019
 """
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import sys
 
+from ui.bojan import Ui_MainWindow
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+        self.stackedWidget.setCurrentIndex(0)
+
+def ui_main():
+    app = QApplication(sys.argv)
+    wnd = MainWindow()
+    wnd.show()
+    sys.exit(app.exec_())
 
 def main():
     """ Main function """
-    pass
-
+    ui_main()
+    
 
 if __name__ == '__main__':
     main()
+
