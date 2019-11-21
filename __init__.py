21115b07ae2581049b8ceedcb500c2cdb5a53e29
Jaka Kovše <jk4684@student-uni-lj.si>
2019-11-21 09:22:52 +0100

    delujoc ui brez funkcij

--- __init__.py
+++ __init__.py
@@ -4,17 +4,25 @@ Bojan interface
 NEMI - 2019
 """
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
+
 import sys
 
+from ui.joystick import Joystick
 from ui.bojan import Ui_MainWindow
+
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
         self.stackedWidget.setCurrentIndex(0)
+        self.joystick = Joystick()                              #naredi joystick widget
+        self.gridLayout_3.addWidget(self.joystick,3,1)
+
+
+#   #   #   definicija funkcij tipk   #   #   #
+
+#   #   #
 
 def ui_main():
     app = QApplication(sys.argv)
@@ -25,7 +33,7 @@ def ui_main():
 def main():
     """ Main function """
     ui_main()
-    
+
 
 if __name__ == '__main__':
     main()
